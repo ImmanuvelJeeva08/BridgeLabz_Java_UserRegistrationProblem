dda90a31adbfa14ac2b5ddaf61900466b2561219
immanuveljeeva08 <immanuveljeeva2000@gmail.com>
2021-07-31 23:59:39 -0700

    Sucessfully Added

---
+++ Uc5.java
@@ -0,0 +1,31 @@
+﻿/***********************************************************************************************************************************************************************
+ *
+ * As a User need to follow pre-defined Password rules.
+ * Rule1 –> minimum 8 Characters
+ * NOTE – All rules must be passed
+ *
+ * @author : Immanuvel Jeeva
+ * @since  : 01-08-2021
+ *
+ *********************************************************************************************************************************************************************/
+package com;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Uc5 {
+
+    @Test
+    void whenPasswordGiven_CheckMin8Char_whenProper_ShouldreturnTrue() {
+        UserRegistration userRegistration= new UserRegistration();
+        boolean output = userRegistration.CharacterValidator("immanuvel");
+        Assertions.assertTrue(output);      // true
+    }
+
+    @Test
+    void whenPasswordGiven_CheckMin8Char_whenNotProper_ShouldreturnFalse() {
+        UserRegistration userRegistration= new UserRegistration();
+        boolean output = userRegistration.CharacterValidator("imman");
+        Assertions.assertFalse(output);     // false
+    }
+}
